6d1fc619c0ccc032c1dd0e0bad4abc0890cd0f08
Murat Gözel <murat@gozel.com.tr>
2020-09-06 13:16:26 +0300

    fixed naming issue within the google analytics service.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import LighthouseAnalytics from './client.js'
-import LighthouseServiceGoogleAnalytics from './services/googleAnalytics.js'
+import LAGoogleAnalytics from './services/googleAnalytics.js'
 
-export {LighthouseAnalytics, LighthouseServiceGoogleAnalytics}
+export {LighthouseAnalytics, LAGoogleAnalytics}
